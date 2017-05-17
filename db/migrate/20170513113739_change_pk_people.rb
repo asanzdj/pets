20a07f93d2e9a1870acad7f1ae2e8a7156b8ad48
Andrea Sanz <asanzdj@gmail.com>
2017-05-17 09:34:10 +0200

    Change migrations

--- db/migrate/20170513113739_change_pk_people.rb
+++ db/migrate/20170513113739_change_pk_people.rb
@@ -1,9 +1,17 @@
 class ChangePkPeople < ActiveRecord::Migration
+
   def change
-    execute('ALTER TABLE pets_development.people MODIFY COLUMN ID INT;');
-    execute('ALTER TABLE pets_development.people DROP PRIMARY KEY;');
-    execute('ALTER TABLE pets_development.people ADD PRIMARY KEY (dni);');
-    execute('ALTER TABLE pets_development.people MODIFY COLUMN dni VARCHAR(9) NOT NULL DEFAULT " ";');
-    execute('ALTER TABLE pets_development.people DROP COLUMN id;');
+    # execute('ALTER TABLE pets_development.people MODIFY COLUMN ID INT;');
+    # execute('ALTER TABLE pets_development.people DROP PRIMARY KEY;');
+    # execute('ALTER TABLE pets_development.people ADD PRIMARY KEY (dni);');
+    # execute('ALTER TABLE pets_development.people MODIFY COLUMN dni VARCHAR(9) NOT NULL DEFAULT " ";');
+    # execute('ALTER TABLE pets_development.people DROP COLUMN id;');
+
+    execute('ALTER TABLE people MODIFY COLUMN ID INT;');
+    execute('ALTER TABLE people DROP PRIMARY KEY;');
+    execute('ALTER TABLE people ADD PRIMARY KEY (dni);');
+    execute('ALTER TABLE people MODIFY COLUMN dni VARCHAR(9) NOT NULL DEFAULT " ";');
+    execute('ALTER TABLE people DROP COLUMN id;');
   end
+
 end
--- db/migrate/20170515133026_change_table_pet.rb
+++ db/migrate/20170515133026_change_table_pet.rb
@@ -1,6 +1,10 @@
 class ChangeTablePet < ActiveRecord::Migration
   def change
-    execute('ALTER TABLE pets_development.pets MODIFY sex VARCHAR(10);');
-    execute('ALTER TABLE pets_development.pets MODIFY type VARCHAR(20);');
+    # execute('ALTER TABLE pets_development.pets MODIFY sex VARCHAR(10);');
+    # execute('ALTER TABLE pets_development.pets MODIFY type VARCHAR(20);');
+
+    execute('ALTER TABLE pets MODIFY sex VARCHAR(10);');
+    execute('ALTER TABLE pets MODIFY type VARCHAR(20);');
+
   end
 end
