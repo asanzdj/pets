895941ba9997a1010d4dc51884a7ee2a55aa0ee6
Andrea Sanz <asanzdj@gmail.com>
2017-05-27 17:03:18 +0200

    Fixed database migrations

--- db/migrate/20170518181347_image_pets.rb
+++ db/migrate/20170518181347_image_pets.rb
@@ -2,6 +2,10 @@ class ImagePets < ActiveRecord::Migration
   def self.up
     change_table :pets do |t|
       t.attachment :picture
+        :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+        :url => "/system/:attachment/:id/:style/:filename",
+        :styles => { :medium => "300x300>", :thumb => "100x100>" },
+        :default_url => ":rails_root/public/system/images/default.gif"
     end
   end
 
--- db/migrate/20170521151602_change_fk.rb
+++ db/migrate/20170521151602_change_fk.rb
@@ -1,6 +1,6 @@
 class ChangeFk < ActiveRecord::Migration
   def change
-    execute ('ALTER TABLE pets_development.pets DROP FOREIGN KEY pets_ibfk_1;')
+    execute ('ALTER TABLE pets DROP FOREIGN KEY pets_ibfk_1;')
     execute('ALTER TABLE pets ADD CONSTRAINT FOREIGN KEY(owner) REFERENCES people(dni) ON DELETE CASCADE;');
   end
 end
