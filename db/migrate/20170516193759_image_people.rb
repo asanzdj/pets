c661c7f1575539dc9d5ea0f919b545b73cd4170d
Andrea Sanz <asanzdj@gmail.com>
2017-05-27 18:05:07 +0200

    Changed migration

--- db/migrate/20170516193759_image_people.rb
+++ db/migrate/20170516193759_image_people.rb
@@ -2,10 +2,6 @@ class ImagePeople < ActiveRecord::Migration
   def self.up
     change_table :people do |t|
       has_attached_file :picture,
-        :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-        :url => "/system/:attachment/:id/:style/:filename",
-        :styles => { :medium => "300x300>", :thumb => "100x100>" },
-        :default_url => ":rails_root/public/system/images/default.gif"
     end
   end
 
